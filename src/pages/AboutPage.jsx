2fba0f688c3c4f12be7262a3f7426e50178cd07c
Yanneuille <yannlg1409@gmail.com>
2025-06-13 00:07:41 +0200

    Normalize line endings to LF

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -41,10 +41,7 @@
                 className="relative group"
               >
                 <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-25 group-hover:opacity-50 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-                <img  
-                  className="relative rounded-lg shadow-2xl w-full h-auto max-h-[650px] object-cover" 
-                  alt="Photo de profil"
-                 src="/dist/assets/PhotoYann.jpg" />
+
               </motion.div>
               <motion.div
                 initial={{ opacity: 0, x: 50 }}
